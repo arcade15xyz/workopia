edbf1908f512fe17afef4007796b82becdc05078
arcade15xyz <arcade15xyz@gmail.com>
2025-02-17 11:46:14 +0530

    creating the views

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ require '../helpers.php';
 $routes = [
     '/' =>  'controllers/home.php',
     '/listings' => 'controllers/listings/index.php',
-    '/listing/create' => 'controllers/listings/create.php',
+    '/listings/create' => 'controllers/listings/create.php',
     '404' => 'controllers/error/404.php'
 ];
 
