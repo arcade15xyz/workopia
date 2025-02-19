d097b8a75008de3471c11a8ab3800a979d632684
arcade15xyz <arcade15xyz@gmail.com>
2025-02-19 11:32:11 +0530

    composer and psr-4 autoloader

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'arcade15xyz/workopia',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e982abf3582e0dad0615dcc3a774c8178057677a',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'arcade15xyz/workopia' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e982abf3582e0dad0615dcc3a774c8178057677a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
