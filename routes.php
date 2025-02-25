57f9ce116049d36c05b8fa8ef382f1bbb5b856d4
arcade15xyz <arcade15xyz@gmail.com>
2025-02-25 17:02:35 +0530

    User Controller and Views

--- routes.php
+++ routes.php
@@ -12,12 +12,15 @@
 // $router->get('/listings/create','controllers/listings/create.php');
 // $router->get('/listing', 'controllers/listings/show.php');
 
-$router->get('/','HomeController@index');
-$router->get('/listings','ListingController@index');
-$router->get('/listings/create','ListingController@create');
-$router->get('/listings/edit/{id}','ListingController@edit');
-$router->get('/listings/{id}','ListingController@show');
+$router->get('/', 'HomeController@index');
+$router->get('/listings', 'ListingController@index');
+$router->get('/listings/create', 'ListingController@create');
+$router->get('/listings/edit/{id}', 'ListingController@edit');
+$router->get('/listings/{id}', 'ListingController@show');
 
-$router->post('/listings','ListingController@store');
-$router->put('/listings/{id}','ListingController@update');
-$router->delete('/listings/{id}','ListingController@destroy');
+$router->post('/listings', 'ListingController@store');
+$router->put('/listings/{id}', 'ListingController@update');
+$router->delete('/listings/{id}', 'ListingController@destroy');
+
+$router->get('/auth/register', 'UserController@create');
+$router->get('/auth/login', 'UserController@login');
